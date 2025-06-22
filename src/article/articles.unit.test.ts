3a321b0c9579dcd3af42defa8b698b09e9c839e3
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-22 16:11:40 +0200

    Refactor unit tests for `articles`, `authors`, and `categories` to improve consistency and readability. Add `pathToContent` validation for articles.

--- src/article/articles.unit.test.ts
+++ src/article/articles.unit.test.ts
@@ -1,59 +1,22 @@
-import { Articles } from "./articles"
-import { describe, expect, test } from "@jest/globals"
-describe("create article data", () => {
-  test("ensure article ID uniqueness", () => {
-    const ids = Articles.map((article) => article.id)
-    const uniqueIds = new Set(ids)
-    expect(uniqueIds.size).toBe(ids.length)
-  })
+import {describe, expect, test} from "@jest/globals"
+import {articles} from "@/src/article/articles";
 
-  test("ensure article slug uniqueness", () => {
-    const slugs = Articles.map((article) => article.slug)
-    const uniqueSlugs = new Set(slugs)
-    expect(uniqueSlugs.size).toBe(slugs.length)
-  })
-
-  test("ensure exactly one article is featured", () => {
-    const isFeaturedArticle = Articles.filter((article) => article.isFeatured)
-    expect(isFeaturedArticle.length).toBe(1)
-  })
-
-  test("ensure all required fields are present and have their correct types", () => {
-    Articles.forEach((article) => {
-      expect(article).toHaveProperty("id")
-      expect(typeof article.id).toBe("string")
-      expect(article).toHaveProperty("title")
-      expect(typeof article.title).toBe("string")
-      expect(article).toHaveProperty("date")
-      expect(typeof article.date).toBe("string")
-      expect(article).toHaveProperty("slug")
-      expect(typeof article.slug).toBe("string")
-      expect(article).toHaveProperty("isFeatured")
-      expect(typeof article.isFeatured).toBe("boolean")
-      expect(article).toHaveProperty("readTime")
-      expect(typeof article.readTime).toBe("number")
-      expect(article).toHaveProperty("description")
-      expect(typeof article.description).toBe("string")
-      expect(article).toHaveProperty("author")
-      expect(typeof article.author).toBe("string")
-      expect(article).toHaveProperty("pathToContent")
-      expect(typeof article.pathToContent).toBe("string")
-      expect(article).toHaveProperty("coverImage")
-      expect(typeof article.coverImage).toBe("string")
-      expect(article).toHaveProperty("tags")
-      expect(Array.isArray(article.categories)).toBe(true)
+describe("article collection", () => {
+    test("ensure article ID uniqueness", () => {
+        const ids = articles.map((article) => article.id)
+        const uniqueIds = new Set(ids)
+        expect(uniqueIds.size).toBe(ids.length)
     })
-  })
 
-  test("ensure pathToContent points to valid paths", () => {
-    Articles.forEach((article) => {
-      expect(article.pathToContent).toMatch(/^\/.*\.md$/)
+    test("ensure article slug uniqueness", () => {
+        const slugs = articles.map((article) => article.slug)
+        const uniqueSlugs = new Set(slugs)
+        expect(uniqueSlugs.size).toBe(slugs.length)
     })
-  })
 
-  test("ensure valid dates", () => {
-    Articles.forEach((article) => {
-      expect(new Date(article.date).toString()).not.toBe("Invalid Date")
+    test("ensure pathToContent points to valid paths", () => {
+        articles.forEach((article) => {
+            expect(article.pathToContent).toMatch(/^src\/article\/articles-content\/.*\/.*\.md$/)
+        })
     })
-  })
 })
