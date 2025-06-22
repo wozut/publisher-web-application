e82f46b6d08dc0d83f693b139ad220017ccadb9d
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-22 14:36:58 +0200

    Re-enable `run_tests` job in GitHub Actions workflow and simplify `authors` unit tests by removing redundant name uniqueness check.

--- src/author/authors.unit.test.ts
+++ src/author/authors.unit.test.ts
@@ -1,16 +1,10 @@
-import { Authors } from "./authors"
+import { authors } from "./authors"
 import { describe, expect, test } from "@jest/globals"
 
 describe("create authors data", () => {
   test("ensure author ID uniqueness", () => {
-    const ids = Authors.map((author) => author.id)
+    const ids = authors.map((author) => author.id)
     const uniqueIds = new Set(ids)
     expect(uniqueIds.size).toBe(ids.length)
-  })
-
-  test("ensure author name uniqueness", () => {
-    const names = Authors.map((author) => author.name)
-    const uniqueNames = new Set(names)
-    expect(uniqueNames.size).toBe(names.length)
   })
 })
