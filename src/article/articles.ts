fdc80b3f2a8c7937816e1d2d11140ebde1ad5914
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-22 16:18:10 +0200

    Add unit test to validate author IDs in articles and assign missing author ID in test data.

--- src/article/articles.ts
+++ src/article/articles.ts
@@ -4,7 +4,7 @@ export const articles: Article[] = [
   {
     id: "e4699f3c-10d9-4415-9227-a46e68073598",
     title: "OAuth2.0: a tale of two perspectives",
-    author: "",
+    author: "46aa23ea-d000-4c71-96fb-748ed383ec33",
     categories: [],
     slug: "oauth20-a-tale-of-two-perspectives",
     pathToContent:
