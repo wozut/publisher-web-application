0cacc90f57b20d51d589d606a6ff0229c4203d0b
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-22 16:21:23 +0200

    Handle missing author in `ArticleView` by updating logic to fetch and validate author data. Simplify author name rendering.

--- src/article/ArticleView.tsx
+++ src/article/ArticleView.tsx
@@ -1,8 +1,12 @@
 import {Article} from "@/src/article/Article";
+import {authors} from "@/src/author/authors";
 
 
 export default async function ArticleView(article: Article) {
-  const authorId = authorRepository
+  const authorId = article.author
+  const author = authors.find((author) => author.id === authorId);
+  if(author === undefined) { throw new Error("Author not found") }
+
   return (
     <div className="w-full flex flex-col place-items-center items-center gap-5">
       <h1 className="w-full lg:font-bold font-semibold lg:text-[64px] text-[30px] leading-tight text-charcoal-blue">
@@ -11,22 +15,9 @@ export default async function ArticleView(article: Article) {
       <div className="w-full h-full flex gap-2 items-center">
         <div className="flex flex-col gap-1">
           <p className="lg:font-semibold font-bold lg:text-xl text-sm text-charcoal-blue">
-            {author[0].name}
+            {author.firstName} {author.lastName}
           </p>
-          <div className="flex gap-2 items-center font-medium lg:text-sm text-[11px] text-gray-500">
-            <p>
-              {article.readTime} {t("readTime")}
-            </p>
-            <p>.</p>
-            <p>{author[0].niche}</p>
-          </div>
         </div>
-      </div>
-      <div className={"w-full"}>
-        <FeaturedArticleThumbnail
-          isFeatured={article.isFeatured}
-          coverImage={article.coverImage}
-        />
       </div>
       <div className="w-full flex flex-col-reverse xl:flex-row gap-8">
         <div className="w-full min-w-[0%] blog-article">
