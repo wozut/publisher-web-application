bb837f293462e66c9c5c82c33b408147308a3745
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-22 13:24:49 +0200

    Add `Article`, `Author`, and `Category` types with corresponding test data and unit tests. Implement article page rendering with `ArticleView` component. Include demo articles and images for initial content population.

---
+++ src/article/articles.ts
@@ -0,0 +1,13 @@
+import {Article} from "@/src/article/Article";
+
+export const articles: Article[] = [
+  {
+    id: "e4699f3c-10d9-4415-9227-a46e68073598",
+    title: "OAuth2.0: a tale of two perspectives",
+    author: "",
+    categories: [],
+    slug: "oauth20-a-tale-of-two-perspectives",
+    pathToContent:
+        "/oauth20-a-tale-of-two-perspectives/oauth20-a-tale-of-two-perspectives.md",
+  }
+] as const
---
+++ src/category/categories.ts
@@ -0,0 +1,9 @@
+import {Category} from "@/src/category/Category";
+
+
+export const categories: Category[] = [
+  {
+    id: "e4699f3c-10d9-4415-9227-a46e68073598",
+    name: ""
+  }
+] as const
