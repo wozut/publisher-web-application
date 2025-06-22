7feb9d69d717f95a8677ab151df5ce063f03356e
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-22 16:11:40 +0200

    Refactor unit tests for `articles`, `authors`, and `categories` to improve consistency and readability. Add `pathToContent` validation for articles.

--- src/author/authors.unit.test.ts
+++ src/author/authors.unit.test.ts
@@ -1,7 +1,7 @@
 import { authors } from "./authors"
 import { describe, expect, test } from "@jest/globals"
 
-describe("create authors data", () => {
+describe("author collection", () => {
   test("ensure author ID uniqueness", () => {
     const ids = authors.map((author) => author.id)
     const uniqueIds = new Set(ids)
