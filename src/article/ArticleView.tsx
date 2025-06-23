d7ad2305d4dec4cfd94df85fa4f7f3b53d2451d1
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-23 02:06:50 +0200

    Add `safeReadFile` utility, enhance `ArticleView` logic to read article content, and update categories and articles data structure. Include new dependencies in `package.json`.

--- src/article/ArticleView.tsx
+++ src/article/ArticleView.tsx
@@ -1,32 +1,40 @@
 import {Article} from "@/src/article/Article";
 import {authors} from "@/src/author/authors";
+import {safeReadFile} from "@/libraries/safeReadFile";
+import {articlesDirectory} from "@/src/article/articlesDirectory";
+import {AnyCustomError} from "@/src/CustomError";
+import {ResultAsync} from "neverthrow";
 
 
 export default async function ArticleView(article: Article) {
-  const authorId = article.author
-  const author = authors.find((author) => author.id === authorId);
-  if(author === undefined) { throw new Error("Author not found") }
-
-  return (
-    <div className="w-full flex flex-col place-items-center items-center gap-5">
-      <h1 className="w-full lg:font-bold font-semibold lg:text-[64px] text-[30px] leading-tight text-charcoal-blue">
-        {article.title}
-      </h1>
-      <div className="w-full h-full flex gap-2 items-center">
-        <div className="flex flex-col gap-1">
-          <p className="lg:font-semibold font-bold lg:text-xl text-sm text-charcoal-blue">
-            {author.firstName} {author.lastName}
-          </p>
-        </div>
-      </div>
-      <div className="w-full flex flex-col-reverse xl:flex-row gap-8">
-        <div className="w-full min-w-[0%] blog-article">
-          <RenderMarkdownText markdownText={markdownContent} />
-        </div>
-        <div className="xl:min-w-[29.7%] xl:w-[29.7%] xl:max-w-[29.7%]">
-          <BlogTableOfContent />
-        </div>
-      </div>
-    </div>
-  )
+    const authorId = article.author
+    const author = authors.find((author) => author.id === authorId);
+    if (author === undefined) {
+        throw new Error("Author not found")
+    }
+    const fullArticlePath = `${articlesDirectory}${article.pathToContent}`;
+    const result: ResultAsync<string | Buffer<ArrayBufferLike>, AnyCustomError> = safeReadFile(fullArticlePath, {encoding: "utf8"})
+    return result.match((content) => {
+        return (
+            <div className="w-full flex flex-col place-items-center items-center gap-5">
+                <h1 className="w-full lg:font-bold font-semibold lg:text-[64px] text-[30px] leading-tight text-charcoal-blue">
+                    {article.title}
+                </h1>
+                <div className="w-full h-full flex gap-2 items-center">
+                    <div className="flex flex-col gap-1">
+                        <p className="lg:font-semibold font-bold lg:text-xl text-sm text-charcoal-blue">
+                            {author.firstName} {author.lastName}
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full flex flex-col-reverse xl:flex-row gap-8">
+                    <div className="w-full min-w-[0%] blog-article">
+                        <RenderMarkdownText markdownText={content}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }, () => {
+        throw new Error("TODO")
+    })
 }
