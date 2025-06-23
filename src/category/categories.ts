aad43152448dc8b2eee65b66b8810fe1d60ddc3c
David Gracia <david.gracia.celemendi@gmail.com>
2025-06-23 02:06:50 +0200

    Add `safeReadFile` utility, enhance `ArticleView` logic to read article content, and update categories and articles data structure. Include new dependencies in `package.json`.

--- src/category/categories.ts
+++ src/category/categories.ts
@@ -4,6 +4,26 @@ import {Category} from "@/src/category/Category";
 export const categories: Category[] = [
   {
     id: "e4699f3c-10d9-4415-9227-a46e68073598",
-    name: ""
+    name: "Spring Boot"
+  },
+  {
+    id: "9809b26b-6df7-4e58-ae5b-8f28501bcbde",
+    name: "Continuous Deployment"
+  },
+  {
+    id: "34849121-c0f6-4c65-bbf9-b500d92d0878",
+    name: "Docker"
+  },
+  {
+    id: "b450a9e7-1015-4966-9ee4-935db1bc4cd2",
+    name: "PaaS"
+  },
+  {
+    id: "0cc01d7d-85b9-4883-b0fc-01fdfac9eeef",
+    name: "Render"
+  },
+  {
+    id: "d3d69358-f54d-4560-8104-c65bd3fc2fc4",
+    name: "GitHub"
   }
 ] as const
