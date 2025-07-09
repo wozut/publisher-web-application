9f9483ca80889e4eff8019245199b97f34a3cc49
David Gracia <david.gracia.celemendi@gmail.com>
2025-07-10 00:37:22 +0200

    Remove `eslint.ignoreDuringBuilds` from `next.config.ts` and add `@types/react-syntax-highlighter` dependency.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
