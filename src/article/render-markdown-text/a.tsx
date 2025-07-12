ba9ac26b51301019b49e4d639771fa7054b901b4
David Gracia <david.gracia.celemendi@gmail.com>
2025-07-12 04:34:58 +0200

    Add `react-icons` library and update `<A>` component to include link icons for improved visual cues.

--- src/article/render-markdown-text/a.tsx
+++ src/article/render-markdown-text/a.tsx
@@ -1,5 +1,16 @@
 import { AnchorHTMLAttributes, ClassAttributes, ReactElement } from "react";
 import { ExtraProps } from "react-markdown";
+import { MdOpenInNew } from "react-icons/md";
+import { MdLink } from "react-icons/md";
+
+function getLinkIcon(
+  target: "_self" | "_blank" | "_parent" | "_top" | (string & {}) | undefined,
+) {
+  const className = "self-start w-4";
+  if (target === "_blank") return <MdOpenInNew className={className} />;
+  if (target === "_self") return <MdLink className={className} />;
+  return <span className={"hidden"}></span>;
+}
 
 export function A({
   children,
@@ -11,12 +22,17 @@ export function A({
   ExtraProps): ReactElement {
   return (
     <a
-      className={"font-semibold text-charcoal-blue hover:drop-shadow-cs"}
+      className={
+        "font-medium hover:drop-shadow-[0_0_0.025rem_rgba(0,0,0,1)]/100"
+      }
       href={href}
       target={target}
       {...rest}
     >
-      {children}
+      <span className={"inline-flex flex-row"}>
+        <span className={"self-baseline"}>{children}</span>
+        {getLinkIcon(target)}
+      </span>
     </a>
   );
 }
