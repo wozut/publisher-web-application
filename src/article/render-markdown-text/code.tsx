140ac79d52aa73146b6f13a6959c33291526f30b
David Gracia <david.gracia.celemendi@gmail.com>
2025-07-20 17:05:50 +0200

    Refactor `<code>` component: adjust padding, background, and text styling for improved consistency and alignment with design standards.

--- src/article/render-markdown-text/code.tsx
+++ src/article/render-markdown-text/code.tsx
@@ -12,7 +12,7 @@ export function Code({
   ExtraProps): ReactElement {
   return (
     <code
-      className={`${jetBrainsMono.className} code-text px-1 py-0.5 bg-gray-300 rounded-[0.25rem]`}
+      className={`${jetBrainsMono.className} code-text px-0.5 bg-gray-200 rounded-[0.25rem] text-nowrap`}
       {...rest}
     >
       {children}
