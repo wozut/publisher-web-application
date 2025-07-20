1df217393b1c3bc3c6030bd0cff986ca3fd96f40
David Gracia <david.gracia.celemendi@gmail.com>
2025-07-20 17:39:40 +0200

    Refactor `<code>` component: remove `JetBrains_Mono` font import, centralize font logic in CSS with `--font-code`, and simplify class usage for consistency.

--- src/article/render-markdown-text/code.tsx
+++ src/article/render-markdown-text/code.tsx
@@ -1,8 +1,5 @@
-import { JetBrains_Mono } from "next/font/google";
 import { ClassAttributes, HTMLAttributes, ReactElement } from "react";
 import { ExtraProps } from "react-markdown";
-
-const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 export function Code({
   children,
@@ -11,10 +8,7 @@ export function Code({
   HTMLAttributes<HTMLElement> &
   ExtraProps): ReactElement {
   return (
-    <code
-      className={`${jetBrainsMono.className} code-text px-0.5 bg-code-background rounded-[0.25rem] text-nowrap`}
-      {...rest}
-    >
+    <code className={`code`} {...rest}>
       {children}
     </code>
   );
