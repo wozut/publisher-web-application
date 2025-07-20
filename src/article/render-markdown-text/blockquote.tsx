17c331ed3a437d78766d12f0fb8a5f4c096ee910
David Gracia <david.gracia.celemendi@gmail.com>
2025-07-20 12:36:28 +0200

    Refactor `<blockquote>` component: simplify and clean up typography-related class usage for improved readability and maintainability.

--- src/article/render-markdown-text/blockquote.tsx
+++ src/article/render-markdown-text/blockquote.tsx
@@ -8,20 +8,14 @@ export function Blockquote({
   ExtraProps): ReactElement {
   return (
     <blockquote className="flex flex-row w-full gap-1 article-block-margin-y">
-      <span
-        className={
-          "w-fit h-[1.4rem] sm:h-[2.5rem] self-start font-semibold text-double-quote"
-        }
-      >
-        “
-      </span>
-      <span className="overflow-x-auto regular-article-text pt-1 sm:pt-2 text-start w-full">
+      <span className={"self-start font-semibold text-double-quote"}>“</span>
+      <span className="overflow-x-auto regular-article-text text-start w-full">
         {children}
       </span>
 
       <span
         className={
-          "w-fit h-[2.3rem] sm:h-[2.5rem] self-end font-semibold text-double-quote"
+          "h-[2.3rem] sm:h-[2.5rem] self-end font-semibold text-double-quote"
         }
       >
         ”
