d6b523b48a2298a2b264166336cd6e8bf06935bd
David Gracia <david.gracia.celemendi@gmail.com>
2025-07-24 02:23:50 +0200

    Add `RenderHtmlText` component and integrate `html-react-parser` library for rendering HTML strings in React.

---
+++ src/article/render-markdown-text/render-html-text.tsx
@@ -0,0 +1,35 @@
+import { ReactElement } from "react";
+import parse, {
+  HTMLReactParserOptions,
+  Element,
+  domToReact,
+  DOMNode,
+} from "html-react-parser";
+import { markUpLanguageToComponentMap } from "./markup-language-to-component-map";
+import { isUndefined } from "@/libraries/value-definition/isUndefined";
+
+export function RenderHtmlText({
+  htmlText,
+}: {
+  htmlText: string;
+}): ReactElement {
+  const options: HTMLReactParserOptions = {
+    replace: (domNode) => {
+      if (!(domNode instanceof Element) || domNode.type !== "tag")
+        return undefined;
+
+      const tagName = domNode.name;
+      const CustomComponent = markUpLanguageToComponentMap[tagName];
+
+      if (isUndefined(CustomComponent)) return undefined;
+
+      const props = {
+        ...domNode.attribs,
+        children: domToReact(domNode.children as DOMNode[], options),
+      };
+      return <CustomComponent {...props} />;
+    },
+  };
+
+  return <>{parse(htmlText, options)}</>;
+}
